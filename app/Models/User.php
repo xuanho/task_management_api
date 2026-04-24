5705e972c6c754633302973aaf150c830be47e3b
Xuan Ho <xuan@javactechnology.com>
2026-04-24 15:41:32 +0700

    PSR

--- app/Models/User.php
+++ app/Models/User.php
@@ -30,7 +30,8 @@ class User extends Authenticatable
         'remember_token',
     ];
 
-    protected function casts(): array{
+    protected function casts(): array
+    {
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
